780c94b2c8fbf30e3d5d73da414fe36e1b0e58f9
M1nZof <103283157+M1nZof@users.noreply.github.com>
2023-01-31 19:33:40 +0300

    Added argparse and TG connect

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+import argparse
+import telegram
 import os
+from dotenv import load_dotenv
+
+from fetch_spacex_images import fetch_spacex_last_launch
+from nasa_epic import nasa_epic
+from nasa_picture_of_the_day import nasa_picture_of_the_day
 
 
+load_dotenv()
+TG_TOKEN = os.environ['TG_TOKEN']
+bot = telegram.Bot(token=TG_TOKEN)
 
+parser = argparse.ArgumentParser(description='Программа загружает фото запусков SpaceX, фото земли от NASA или фото дня от NASA')
+parser.add_argument('search',
+                    help='Что искать:'
+                    '- sx - Фото от SpaceX. Если требуется, указать id пуска по аргументу -id. По стандарту это последний запуск'
+                    '- epic - Фото земли от NASA'
+                    '- apod - Фото дня от NASA',
+                    type=str
+                   )
+parser.add_argument('-id', help='ID вылета SpaceX', type=str)
+parser.add_argument('-p', '--path', type=str,
+                    help='Путь сохранения файлов. Стандартно - папка images')
+args = parser.parse_args()
 
+if args.search == 'sx':
+  fetch_spacex_last_launch(args.path, args.id)
+elif args.search == 'epic':
+  nasa_epic()
+elif args.search == 'apod':
+  nasa_picture_of_the_day()
