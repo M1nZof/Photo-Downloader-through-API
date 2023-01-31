a50b47bd067b9fe2b82b49f70cdfaecdc25e4a0b
M1nZof <103283157+M1nZof@users.noreply.github.com>
2023-01-31 20:45:13 +0300

    Revert "Ignore images directory in .gitignore"
    This reverts commit fd60c0a9ec18cf02eef49b207fad5d0672dea2a9.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import argparse
 import telegram
 import os
-
-from time import sleep
-from random import shuffle
 from dotenv import load_dotenv
 
 from fetch_spacex_images import fetch_spacex_last_launch
@@ -14,25 +11,15 @@ from nasa_picture_of_the_day import nasa_picture_of_the_day
 load_dotenv()
 TG_TOKEN = os.environ['TG_TOKEN']
 bot = telegram.Bot(token=TG_TOKEN)
-
-
-image_directory = os.walk('images')
-for dir_name, _, image in image_directory:
-    image_list = image
-    
-
-shuffle(image_list)
-for image in image_list:
-    bot.send_photo(chat_id='@testttttttttttttasdasfsdf', photo=open(f'images/{image}', 'rb'))
-    sleep(14400)
+bot.send_photo(chat_id='@testttttttttttttasdasfsdf', photo=open('images/EPIC_0.png', 'rb'))
 
 
 parser = argparse.ArgumentParser(description='Программа загружает фото запусков SpaceX, фото земли от NASA или фото дня от NASA')
 parser.add_argument('search',
                     help='Что искать:'
-                    '- sx - Фото от SpaceX. Если требуется, указать id пуска по аргументу -id. По стандарту это последний запуск; '
-                    'epic - Фото земли от NASA; '
-                    'apod - Фото дня от NASA;',
+                    '- sx - Фото от SpaceX. Если требуется, указать id пуска по аргументу -id. По стандарту это последний запуск'
+                    '- epic - Фото земли от NASA'
+                    '- apod - Фото дня от NASA',
                     type=str
                    )
 parser.add_argument('-id', help='ID вылета SpaceX', type=str)
