0961ffb17a09746df35979d60be56bb74b0ce7d2
M1nZof <103283157+M1nZof@users.noreply.github.com>
2023-01-31 19:32:59 +0300

    Deleted test lines

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import requests
 import os
-    
 
-# url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-# image_dir_name = 'images'
-# image_download(url, image_dir_name, 'hubble')
 
-# img_dir = 'images'
-# fetch_spacex_last_launch(img_dir, 'spacex')
 
-# nasa_picture_of_the_day('images')
 
-nasa_epic()
